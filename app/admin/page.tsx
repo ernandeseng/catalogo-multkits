a030c8d74688e94260d3174067fd92d7c0f1f846
setortecnico1 <setortecnico1@setortecnico01.casmed.com>
2025-11-28 16:22:25 -0300

    Remove old admin folder causing build error

--- app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from './LoginForm';
-
-export default function AdminPage() {
-    return (
-        <main className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
-            <LoginForm />
-        </main>
-    );
-}
